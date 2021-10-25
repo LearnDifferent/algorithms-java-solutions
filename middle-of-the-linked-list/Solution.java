0677cceb66a815562656d3de70423be238b8627a
zhou <imzhouhepeng@outlook.com>
2021-10-25 19:34:12 +0800

    feat: 867. middle-of-the-linked-list

---
+++ middle-of-the-linked-list/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * 876. Middle of the Linked List:
+ *  链表的中间结点
+ *
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ * 链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        return slow;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) { this.val = val; }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
