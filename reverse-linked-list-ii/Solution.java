3ccf6c7cab8e0f48a1a55ae7d7428e5d1aebca33
zhou <imzhouhepeng@outlook.com>
2021-10-27 22:25:07 +0800

    feat: modify 92. Reverse Linked List II

--- reverse-linked-list-ii/Solution.java
+++ reverse-linked-list-ii/Solution.java
@@ -1,17 +1,11 @@
 /**
  * 92. Reverse Linked List II:
+ * 反转链表 II
+ * 解决方案：递归
+ *
  * Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
  *
  * 链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
- *
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
 class Solution {
 
@@ -23,7 +17,9 @@ class Solution {
             return reverseFirstToN(head, right);
         }
 
-        // 反转当前节点之后的节点
+        // 反转当前节点之后的节点:
+		// 因为是从第二个节点开始反转，所以缩小的范围应该整体向前移动
+		// 可以理解为起始点向后移动了，所以保存原来的范围就需要让范围向前移动
         ListNode nextNodeAfterReversed = reverseBetween(head.next, left - 1, right - 1);
         // 重新设置当前节点的下一个节点
         head.next = nextNodeAfterReversed;
@@ -31,7 +27,9 @@ class Solution {
         return head;
     }
 
-    // successor
+    // 获取反转之后，最后一个节点后面的节点 successor
+	// 因为只是反转区间内的节点，反转了之后，
+	// 其最后一个节点还需要和 successor 相连接
     ListNode succ = null;
 
     // 反转第一个到第 N 个
@@ -53,4 +51,15 @@ class Solution {
 
         return result;
     }
+}
+
+/*
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
