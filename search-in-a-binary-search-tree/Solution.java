5de6b639d5401d8e92517fc456285ac39a4ea2b1
zhou <imzhouhepeng@outlook.com>
2021-11-04 15:27:33 +0800

    feat: search-in-a-binary-search-tree

---
+++ search-in-a-binary-search-tree/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * 700. Search in a Binary Search Tree:
+ *
+ * You are given the root of a binary search tree (BST) and an integer val.
+ *
+ * Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+ * If such a node does not exist, return null.
+ *
+ * 链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+ */
+class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return root;
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) { this.val = val; }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
