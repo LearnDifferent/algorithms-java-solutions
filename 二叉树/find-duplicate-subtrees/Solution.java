4133933c27deef37eb72e5a679d894a7f6d58327
zhou <imzhouhepeng@outlook.com>
2021-11-18 14:08:51 +0800

    refactor: find-duplicate-subtrees

--- 二叉树/find-duplicate-subtrees/Solution.java
+++ 二叉树/find-duplicate-subtrees/Solution.java
@@ -20,30 +20,32 @@ import java.util.Map;
 class Solution {
 
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        serializeWithPostorderTraversal(root);
+        Map<String, Integer> countSubtrees = new HashMap<>();
+        List<TreeNode> result = new ArrayList<>();
+
+        serializeWithPostorderTraversal(root, countSubtrees, result);
         return result;
     }
-
-    // 所有序列化后的子树，以及出现的次数
-    Map<String, Integer> countSubtrees = new HashMap<>();
-    // 需要返回的答案：重复的子树的根节点
-    List<TreeNode> result = new ArrayList<>();
 
     /**
      * Serialize Binary Tree with Post-order Traversal
      *
-     * @param root 需要被序列化的根节点
+     * @param root          需要被序列化的根节点
+     * @param countSubtrees 所有序列化后的子树，以及出现的次数
+     * @param result        需要返回的答案：重复的子树的根节点
      * @return 根据后续遍历，完成来序列化的二叉树
      */
-    private String serializeWithPostorderTraversal(TreeNode root) {
+    private String serializeWithPostorderTraversal(TreeNode root,
+                                                   Map<String, Integer> countSubtrees,
+                                                   List<TreeNode> result) {
         if (root == null) {
             // 如果 root 为 null，序列化为：#
             return "#";
         }
 
         // 序列化左右子树
-        String left = serializeWithPostorderTraversal(root.left);
-        String right = serializeWithPostorderTraversal(root.right);
+        String left = serializeWithPostorderTraversal(root.left, countSubtrees, result);
+        String right = serializeWithPostorderTraversal(root.right, countSubtrees, result);
 
         // 序列化当前这个树，格式为：根节点的值,左子树,右子树
         String subtree = root.val + "," + left + "," + right;
@@ -52,7 +54,7 @@ class Solution {
         // 获取当前这颗树出现的次数（转化为 int 比较方便）
         int count = countSubtrees.getOrDefault(subtree, 0);
 
-        // 如果只出现一次，就加入
+        // 如果已经出现一次，当前就是第二次出现，也就是重复出现，所以要加入（超过 2 次的话，就没必要加入）
         if (count == 1) {
             result.add(root);
         }
