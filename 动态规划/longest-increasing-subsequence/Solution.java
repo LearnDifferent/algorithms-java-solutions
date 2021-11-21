ce66ec16a87127fb29daedaec7036b0f09134096
zhou <imzhouhepeng@outlook.com>
2021-11-21 11:01:37 +0800

    refactor: longest-increasing-subsequence

--- 动态规划/longest-increasing-subsequence/Solution.java
+++ 动态规划/longest-increasing-subsequence/Solution.java
@@ -17,33 +17,31 @@ import java.util.Arrays;
  * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
  */
 class Solution {
-
     public int lengthOfLIS(int[] nums) {
-        // dp[3] = 2 表示以 nums[3] 数字为结尾的 最长递增子序列 的长度为 2
+        // dp[3] 表示到 nums[3] 为止的 LIS(longest increasing subsequence)
         int[] dp = new int[nums.length];
 
-        // base case：假设每个数字都是不重复的，也就是以每个数字结尾，长度都为 1
+        // base case: 假设每个数字都是不重复的，那么每个位置的 LIS 为 1
         Arrays.fill(dp, 1);
 
         // 遍历 nums 数组中的元素
         for (int i = 0; i < nums.length; i++) {
-            // 遍历当前元素，之前的几个元素
+            // 遍历 nums[i] 之前，数组内的元素，看看有没有比 nums[i] 还小的元素
             for (int j = 0; j < i; j++) {
+                // 如果找到了比 nums[i] 还小的元素，说明可以和 nums[i] 组成 LIS
                 if (nums[i] > nums[j]) {
-                    // 如果当前元素，比当前元素之前的某个元素大，
-                    // 说明当前第 i 个元素，可以从第 j 个元素组成递增子序列，
-                    // 此时，再和当前位置的最优解相比较，最长的那个就是当前新的最优解
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    // 此时可以组成 LIS，LIS 的长度为之前元素位置的最优解 + 当前这 1 个单位
+                    int current = 1 + dp[j];
+                    // 因为会动态更新当前位置的最优解，所以还需要比较一下
+                    dp[i] = Math.max(dp[i], current);
                 }
             }
         }
 
-        // 最后的结果，就是 dp 中的最大值
+        // 在 dp 最后结果中的最大值，就是 LIS
         int max = 0;
-        for (int i : dp) {
-            if (i > max) {
-                max = i;
-            }
+        for (int lis : dp) {
+            max = Math.max(lis, max);
         }
         return max;
     }
